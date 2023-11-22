dd46801a9e9a6f6d36f490514b2bb96bdce32971
wosean <serhii_v@ukr.net>
2023-11-22 23:31:52 +0200

    hw-03 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,71 @@
-const key = new Key();
+// const key = new Key();
 
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
+
+// export {};
+
+class Key {
+    private signature: number;
+    constructor() {
+        this.signature = Math.random();
+    }
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+    constructor(key: Key) {
+        this.key = key;
+    }
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean =false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    abstract openDoor(key: Key): void;
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log('Open door!');
+        } else {
+            console.log('Closed door!');
+       }
+    }
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('Open door!');
+        } else {
+            console.log('Closed door!');
+        }
+    }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
 
-
-export {};
+export { };
